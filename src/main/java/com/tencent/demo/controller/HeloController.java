d4531292f83b2b928d483e9ec45f3f05f3c3d40a
å <dztz663@gmail.com>
2025-03-22 18:12:10 +0800

    添加了工具库和json工具库

--- src/main/java/com/tencent/demo/controller/HeloController.java
+++ src/main/java/com/tencent/demo/controller/HeloController.java
@@ -1,8 +1,11 @@
 package com.tencent.demo.controller;
 
+import com.alibaba.fastjson2.JSON;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tencent.demo.entity.Student;
 import com.tencent.demo.service.StudentService;
 import okhttp3.*;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +21,13 @@ public class HeloController {
 
     @Autowired
     private StudentService studentService;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @GetMapping("/hello")
     public Object hello() throws IOException {
 
-
-        return "execute.body().bytes()";
+        return "success";
     }
 
     @GetMapping("/find")
