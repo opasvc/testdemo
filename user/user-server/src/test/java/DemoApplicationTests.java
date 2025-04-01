c7cf0046c05900d0c698751243b35811ad7119d9
å <dztz663@gmail.com>
2025-04-01 17:30:57 +0800

    测试用例

--- user/user-server/src/test/java/DemoApplicationTests.java
+++ user/user-server/src/test/java/DemoApplicationTests.java
@@ -6,6 +6,22 @@ class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+        String s1 = "hello";
+        String s2 = "world";
+        String s3 = s1 + s2;
+        String s4 = "helloworld";
+        System.out.println(s3 == s4);
+        System.out.println(s3.equals(s4));
     }
 
+    public static void main(String[] args) {
+//        new DemoApplicationTests().contextLoads();
+        String s1 = "hello";
+        String s2 = s1;
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+        s2 = "world";
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+    }
 }
